3aa3f8cd79688bc0221ebe0629d4b284f33b8d2d
tueki <aboody3553@outlook.com>
2024-12-01 04:10:19 +0300

    s

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 ob_start();
 ?>
-<!--
-Name: Abdulelah Ali Alturki
-ID: 2136110
-Date: 9/24/2024
--->
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 
