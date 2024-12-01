79b02ddebb8ea4d2f82bfda4505a6582f45153c9
tueki <aboody3553@outlook.com>
2024-12-01 03:59:34 +0300

    s

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ ID: 2136110
 Date: 9/24/2024
 -->
 
-<!DOCTYPE "XHTML 1.0 Strict">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 
 <head>
