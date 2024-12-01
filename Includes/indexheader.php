a17e678ff4b4009391d039a236d4553dc60c1d27
tueki <aboody3553@outlook.com>
2024-12-01 03:14:30 +0300

    XHTML

--- Includes/indexheader.php
+++ Includes/indexheader.php
@@ -7,36 +7,33 @@ ob_start();
     <div class="header-topline">
         <div class="logoline">
             <!-- Logo link to the index page -->
-            <a href="index.php"><img src="Images/Turki-Bookstore-Logo-Rectangle.png" alt="Turki-Bookstore-Logo" width="150"></a>
+            <a href="index.php"><img src="Images/Turki-Bookstore-Logo-Rectangle.png" alt="Turki-Bookstore-Logo" width="150" /></a>
         </div>
 
         <!-- Search bar input -->
         <div class="searchbar">
             <form class="searchbar" action="backend/search.php" method="get">
-                <input type="text" placeholder="Search.." name="search" autocomplete="off">
+                <input type="text" name="search" />
             </form>
-
         </div>
 
         <!-- Shopping cart link -->
         <div class="cart">
-            <a href="HTML/cart.php"><img src="Images/shopping-cart.png" alt="Cart" width="50"></a>
+            <a href="HTML/cart.php"><img src="Images/shopping-cart.png" alt="Cart" width="50" /></a>
         </div>
 
         <!-- Login and Register buttons -->
-         <?php if(!isset($_SESSION['user_id'])){ ?>
+        <?php if (!isset($_SESSION['user_id'])) { ?>
         <div class="auth-buttons">
             <a href="HTML/loginpage.php" class="button login-btn">Login</a>
             <a href="HTML/registerpage.php" class="button register-btn">Register</a>
         </div>
-        <?php }else { ?> 
+        <?php } else { ?>
         <div class="auth-buttons">
             <span class="welcomename">Welcome, <?php echo htmlspecialchars($_SESSION['fname']); ?>!</span>
             <a href="backend/logout.php" class="button login-btn">Logout</a>
         </div>
         <?php } ?>
-
-        
     </div>
 
     <!-- Navigation menu -->
