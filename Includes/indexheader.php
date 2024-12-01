e9bdb025a3689d2dd0fe4439f51af54a98eb3304
tueki <aboody3553@outlook.com>
2024-12-01 04:04:00 +0300

    s

--- Includes/indexheader.php
+++ Includes/indexheader.php
@@ -2,6 +2,9 @@
 session_start();
 ob_start();
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"></html>
 <!-- Header section starts -->
 <div class="header">
     <div class="header-topline">
